f7c3988a8e5adea36de08fe36a4250d5882316bd
vlbu0001 <113466734+bukh04@users.noreply.github.com>
2024-12-09 11:48:51 +0100

    bannerForCreating

--- Domain Models/Service/IBoligService.cs
+++ Domain Models/Service/IBoligService.cs
@@ -9,9 +9,6 @@ namespace Domain.Models.Service
         List<Property> GetAllPropeties();
 		Task<IEnumerable<Property>> GetPropertyById(int id);
         Task<List<User>> GetUsers();
-        Task<int> CreateAnnonceAsync(Property property);
-        Task AddPicturesAsync(int propertyId, List<Pictures> pictures);
-        Task<List<Pictures>> GetPicturesByPropertyIdAsync(int propertyId);
         Task<int> CreateAnnonceWithPicturesAsync(Property property, List<string> pictureLinks);
     }
 }
